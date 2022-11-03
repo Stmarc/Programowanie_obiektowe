f482ca5042b8c01627d24e4df43a9a80a1fc8e8e
Stmarc <93048031+Stmarc@users.noreply.github.com>
2022-11-03 19:12:14 +0100

    Add files via upload
    Lab1

---
+++ main1.java
@@ -0,0 +1,24 @@
+
+
+public class main1 {
+
+
+    public static void main(String[] args) {
+        Main.lab3_3();
+        Main.lab1_1();
+        Main.lab1_2(1,2);
+        Main.lab1_3(2);
+        Main.lab1_4(3);
+        Main.lab1_5(5);
+        Main.lab1_6(5);
+        Main.lab1_7(5,5,2);
+        Main.lab2_2();
+        Main.lab2_3();
+        Main.lab3_1();
+        Main.lab3_2();
+        Main.lab3_3();
+
+
+    }
+
+}
