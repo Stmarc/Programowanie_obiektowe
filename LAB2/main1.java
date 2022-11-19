922b7c9091dc5d6fc15fa1ef097dd3acce659ac7
Stmarc <93048031+Stmarc@users.noreply.github.com>
2022-11-19 14:02:46 +0100

    Add files via upload

---
+++ LAB2/main1.java
@@ -0,0 +1,19 @@
+
+
+public class main1 {
+
+
+    public static void main(String[] args) {
+        Main.zad1();
+        Main.zad11();
+        Main.zad2();
+        Main.zad3();
+        Main.zad4();
+        Main.zad5();
+        Main.zad6();
+        Main.zad7();
+
+
+    }
+
+}
